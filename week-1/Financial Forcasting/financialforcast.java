0aeb7f8895b7ab7caeb312765fc04e29bc7c91ec
Madhan Sundhar <118243033+Madhans57@users.noreply.github.com>
2025-06-21 19:54:44 +0530

    Update financialforcast.java

--- week-1/Financial Forcasting/financialforcast.java
+++ week-1/Financial Forcasting/financialforcast.java
@@ -1,23 +1,32 @@
 package financialforcast;
 import java.util.*;
 public class forcast {
-	public static double method(double pv,double gr,int y) {
+	public static double iterativemethod(double pv,double gr,int y) {
 		double res= pv;
 		for(int i=0;i<y;i++) {
 			res*=(1+gr);
 		}
 		return res;
 	}
+	public static double recursivemethod(double pv,double gr,int y) {
+		if(y==0) return pv;
+		return recursivemethod(pv,gr,(y-1))*(1+gr);
+	}
 	public static void main(String[] args) {
 		double pv=100000.00;   //Present Value
 		double gr=0.08;        //Growth Rate
 		int y=5;               //Number of years
-		double fv=method(pv,gr,y);   // Future Value
+		double fv=iterativemethod(pv,gr,y);   // Future Value
 		System.out.println("Financial Forcast");
 		System.out.println("=================");
 		System.out.printf("Present Value = $%.2f\n", pv);
 		System.out.printf("Growth Rate per year = %.2f%%\n",(gr*100));
 		System.out.println("Number of years = "+y);
-		System.out.printf("Future Value = $%.2f\n", fv);
+		System.out.println("=================");
+		System.out.printf("Future Value using Iterative Approach = $%.2f\n", fv);
+		
+		double rv=recursivemethod(pv,gr,y); // recursive method
+		System.out.println("=================");
+		System.out.printf("Future Value using Recursive Approach = $%.2f\n", fv);
 	}
 }
